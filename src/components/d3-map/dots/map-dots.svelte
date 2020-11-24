6c59949d081185ba26eecfa86352333eac1cd5e5
Rowin Ruizendaal <rowin@medialab.co>
2020-11-24 11:11:56 +0100

    WIP

--- src/components/d3-map/dots/map-dots.svelte
+++ src/components/d3-map/dots/map-dots.svelte
@@ -1,27 +1,47 @@
 <script>
-  import { select, geoMercator } from 'd3'
+  import { select, geoMercator, geoPath } from 'd3'
   import { onMount } from 'svelte'
 
+  let dots = []
+
+  const projection = geoMercator().scale(6000).center([5.116667, 52.17])
   onMount(async () => {
     const response = await fetch(
       'https://gist.githubusercontent.com/RowinRuizendaal/4db72bcb197b05ac444d281da70110ae/raw/dc3bce0996bc8f0ffb70d06999a8b0cfd1ea75a8/betaalmethode_new.json'
     )
     const json = await response.json()
-    const g = select('svg')
-    const projection = geoMercator().scale(6000).center([5.116667, 52.17])
+    // const g = select('svg')
+    // const projection = geoMercator().scale(6000).center([5.116667, 52.17])
+    dots = json
+    console.log(dots)
 
-    g.selectAll('circle')
-      .data(json)
-      .enter()
-      .append('circle')
-      .attr('class', 'circles')
-      .attr('cx', function (d) {
-        return projection([d.location.longitude, d.location.latitude])[0]
-      })
-      .attr('cy', function (d) {
-        return projection([d.location.longitude, d.location.latitude])[1]
-      })
-      .attr('r', '4px')
-      .attr('fill', '#e94560')
+    // g.selectAll('circle')
+    //   .data(json)
+    //   .enter()
+    //   .append('circle')
+    //   .attr('class', 'circles')
+    //   .attr('cx', function (d) {
+    //     return projection([d.location.longitude, d.location.latitude])[0]
+    //   })
+    //   .attr('cy', function (d) {
+    //     return projection([d.location.longitude, d.location.latitude])[1]
+    //   })
+    //   .attr('r', '4px')
+    //   .attr('fill', '#e94560')
   })
 </script>
+
+<style>
+  .circle {
+    fill: var(--roze);
+    border-radius: 4px;
+  }
+</style>
+
+{#each dots as plot}
+  <circle
+    class="circle"
+    cx={projection([plot.location.longitude, plot.location.latitude])[0]}
+    cy={projection([plot.location.longitude, plot.location.latitude])[1]}
+  />
+{/each}
